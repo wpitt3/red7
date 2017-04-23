843f6d16edc3cf4246d6774ae4ae73bb6f8e0000
Will Pitt <will@carsnip.com>
2017-04-23 01:22:59 +0100

    all rules complete

--- src/main/groovy/red/HandAnalyser.groovy
+++ src/main/groovy/red/HandAnalyser.groovy
@@ -9,7 +9,7 @@ import red.domain.Colour
 class HandAnalyser {
 
     static Integer compareHands(List hands, Colour rule) {
-        Map rules = [(Colour.R): redRule, (Colour.O): orangeRule]
+        Map rules = [(Colour.R): redRule, (Colour.O): orangeRule, (Colour.Y): yellowRule, (Colour.G): greenRule, (Colour.B): blueRule, (Colour.I): indigoRule, (Colour.V): violetRule]
 
         List allCards = hands.flatten()
         if (allCards.size() != (allCards as Set).size()) {
@@ -20,30 +20,89 @@ class HandAnalyser {
     }
 
     static Closure redRule = { List hands ->
-        return winningHandFromRule(sortHands(hands), { it.value[0]} )
+        return getHandsAsMap(sortHands(hands)).max{ it.value[0] }.key
     }
 
     static Closure orangeRule = { List hands ->
-        List handsNumberGroup = hands.collect{findLargestGroupWithSameNumber(it)}
-        Integer maxSize = handsNumberGroup.max{it.size()}.size()
-        handsNumberGroup = handsNumberGroup.collect{ it.size() == maxSize ? it : []}
-        return winningHandFromRule(sortHands(handsNumberGroup), { it.value[0] })
+        List sameNumberHands = hands.collect{findLargestGroupWithSameNumber(it)}
+        return winningHandIndex(sameNumberHands)
     }
 
-    /**
-     * returns largest group (higher number group if draw)
-     */
     static List findLargestGroupWithSameNumber(List hand) {
         return hand.groupBy{Card card -> card.number}
                 .max{group -> group.key + group.value.size()*10}.value
+    }
+
+    static Closure yellowRule = { List hands ->
+        List sameColourHands = hands.collect{findLargestGroupWithSameColour(it)}
+        return winningHandIndex(sameColourHands)
+    }
+
+    static List findLargestGroupWithSameColour(List hand) {
+        return hand.groupBy{Card card -> card.colour.rank}
+                .max{group -> group.key + group.value.size()*10}.value
+    }
+
+    static Closure greenRule = { List hands ->
+        List evenHands = hands.collect{findEvenCards(it)}
+        return evenHands.flatten().size() == 0 ? -1 : winningHandIndex(evenHands)
+    }
+
+    static List findEvenCards(List hand) {
+        return hand.findAll{Card card -> card.number%2 == 0}
+    }
+
+    static Closure blueRule = { List hands ->
+        List rainbowHands = sortHands(hands).collect{findCardsOfEachColour(it)}
+        return winningHandIndex(rainbowHands)
+    }
+
+    static List findCardsOfEachColour(List hand) {
+        return hand.inject([]) {result, toAdd ->
+            result + (result.every{card -> card.colour != toAdd.colour} ? [toAdd] : [])
+        }
+    }
+
+    static Closure indigoRule = { List hands ->
+        List sequentialHands = sortHands(hands).collect{findSequentialCards(it)}
+        return winningHandIndex(sequentialHands)
+    }
+
+    //not 100% sure
+    static List findSequentialCards(List hand) {
+        List groups = [[]]
+        Integer lookingFor = 7
+        hand.each { card ->
+            if (card.number == lookingFor) {
+                groups.last().add(card)
+                lookingFor--
+            } else if(card.number < lookingFor) {
+                while(card.number <= lookingFor) {
+                    lookingFor--
+                }
+                groups.add([card])
+            }
+        }
+        return groups.max{it.size()}
+    }
+
+    static Closure violetRule = { List hands ->
+        List lowNumberHands = hands.collect{findLowNumber(it)}
+        return lowNumberHands.flatten().size() == 0 ? -1 : winningHandIndex(lowNumberHands)
+    }
+
+    static List findLowNumber(List hand) {
+        return hand.findAll{Card card -> card.number < 4}
     }
 
     static List sortHands(List hands) {
         return hands.collect{ hand -> hand.sort().reverse() }
     }
 
-    static Integer winningHandFromRule(List hands, Closure rule) {
-        return getHandsAsMap(hands).max{ rule(it) }.key
+    static Integer winningHandIndex(List hands) {
+        Integer maxSize = hands.max{it.size()}.size()
+        hands = hands.collect{ it.size() == maxSize ? it : []}
+        return getHandsAsMap(sortHands(hands)).max{ it.value[0] }.key
     }
 
     static Map getHandsAsMap(List hands) {
--- src/test/groovy/red/HandAnalyserTest.groovy
+++ src/test/groovy/red/HandAnalyserTest.groovy
@@ -118,4 +118,208 @@ class HandAnalyserTest extends Specification {
         then:
         result == 1
     }
+
+    void "YellowRule most of one colour"() {
+        given:
+        List handA = [new Card(Colour.O, 5), new Card(Colour.B, 7)]
+        List handB = [new Card(Colour.R, 1), new Card(Colour.R, 2)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.Y)
+
+        then:
+        result == 1
+    }
+
+    void "YellowRule higher number if draw"() {
+        given:
+        List handA = [new Card(Colour.B, 5), new Card(Colour.B, 4)]
+        List handB = [new Card(Colour.V, 6), new Card(Colour.V, 1)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.Y)
+
+        then:
+        result == 1
+    }
+
+    void "YellowRule highest colour if numbers draw"() {
+        given:
+        List handA = [new Card(Colour.B, 5), new Card(Colour.B, 6)]
+        List handB = [new Card(Colour.Y, 1), new Card(Colour.Y, 6)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.Y)
+
+        then:
+        result == 1
+    }
+
+    void "GreenRule most even"() {
+        given:
+        List handA = [new Card(Colour.O, 5), new Card(Colour.B, 7)]
+        List handB = [new Card(Colour.O, 1), new Card(Colour.R, 2)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.G)
+
+        then:
+        result == 1
+    }
+
+    void "GreenRule higher number if draw"() {
+        given:
+        List handA = [new Card(Colour.B, 5), new Card(Colour.B, 4)]
+        List handB = [new Card(Colour.V, 6), new Card(Colour.R, 1)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.G)
+
+        then:
+        result == 1
+    }
+
+    void "GreenRule highest colour if numbers draw"() {
+        given:
+        List handA = [new Card(Colour.B, 5), new Card(Colour.B, 6)]
+        List handB = [new Card(Colour.R, 1), new Card(Colour.Y, 6)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.G)
+
+        then:
+        result == 1
+    }
+
+    void "GreenRule draw no even"() {
+        given:
+        List handA = [new Card(Colour.B, 5), new Card(Colour.B, 7)]
+        List handB = [new Card(Colour.R, 1), new Card(Colour.Y, 7)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.G)
+
+        then:
+        result == -1
+    }
+
+    void "BlueRule most different colours"() {
+        given:
+        List handA = [new Card(Colour.R, 5), new Card(Colour.R, 7)]
+        List handB = [new Card(Colour.O, 1), new Card(Colour.R, 2)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.B)
+
+        then:
+        result == 1
+    }
+
+    void "BlueRule higher number if draw"() {
+        given:
+        List handA = [new Card(Colour.V, 5), new Card(Colour.B, 4)]
+        List handB = [new Card(Colour.V, 6), new Card(Colour.R, 1)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.B)
+
+        then:
+        result == 1
+    }
+
+    void "BlueRule highest colour if numbers draw"() {
+        given:
+        List handA = [new Card(Colour.R, 5), new Card(Colour.B, 7)]
+        List handB = [new Card(Colour.R, 1), new Card(Colour.Y, 7)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.B)
+
+        then:
+        result == 1
+    }
+
+    void "IndigoRule most in a row"() {
+        given:
+        List handA = [new Card(Colour.R, 5), new Card(Colour.R, 7)]
+        List handB = [new Card(Colour.O, 1), new Card(Colour.G, 2)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.I)
+
+        then:
+        result == 1
+    }
+
+    void "IndigoRule higher number if draw"() {
+        given:
+        List handA = [new Card(Colour.V, 5), new Card(Colour.B, 4), new Card(Colour.R, 7)]
+        List handB = [new Card(Colour.V, 6), new Card(Colour.R, 5), new Card(Colour.B, 5)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.I)
+
+        then:
+        result == 1
+    }
+
+    void "IndigoRule highest colour if numbers draw"() {
+        given:
+        List handA = [new Card(Colour.Y, 6), new Card(Colour.B, 7)]
+        List handB = [new Card(Colour.R, 6), new Card(Colour.Y, 7)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.I)
+
+        then:
+        result == 1
+    }
+
+    void "VioletRule most below 4"() {
+        given:
+        List handA = [new Card(Colour.R, 3), new Card(Colour.R, 4)]
+        List handB = [new Card(Colour.O, 1), new Card(Colour.R, 2)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.V)
+
+        then:
+        result == 1
+    }
+
+    void "VioletRule higher number if draw"() {
+        given:
+        List handA = [new Card(Colour.V, 2), new Card(Colour.B, 2)]
+        List handB = [new Card(Colour.V, 1), new Card(Colour.R, 3)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.V)
+
+        then:
+        result == 1
+    }
+
+    void "VioletRule highest colour if numbers draw"() {
+        given:
+        List handA = [new Card(Colour.R, 2), new Card(Colour.B, 3)]
+        List handB = [new Card(Colour.R, 1), new Card(Colour.Y, 3)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.V)
+
+        then:
+        result == 1
+    }
+
+    void "VioletRule draw no even"() {
+        given:
+        List handA = [new Card(Colour.B, 5), new Card(Colour.B, 7)]
+        List handB = [new Card(Colour.R, 4), new Card(Colour.Y, 7)]
+
+        when:
+        Integer result = HandAnalyser.compareHands([handA, handB], Colour.V)
+
+        then:
+        result == -1
+    }
 }
